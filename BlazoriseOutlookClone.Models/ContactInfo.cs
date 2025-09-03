23a8547f185faf766507ed4fe19d82e7810a8ac9
Mladen Macanovic <mladen.macanovic@gmail.com>
2025-09-03 13:06:05 +0200

    Add contacts components and data

---
+++ BlazoriseOutlookClone.Models/ContactInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazoriseOutlookClone.Models;
+
+public class ContactInfo
+{
+    public string Key { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Company { get; set; }
+}
